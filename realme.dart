5e8ef4b295bf616add3dded02ab6764d0235c4e6
krupanshi1805 <kssohaliya@gmail.com>
2024-05-12 08:33:16 -0700

    My initial commit for realme

---
+++ realme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class RealME extends StatefulWidget {
+  const RealME({super.key});
+
+  @override
+  State<RealME> createState() => _RealMEState();
+}
+
+class _RealMEState extends State<RealME> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
